b90e7a9cf5483559ebc9bd0198c89ddb1d45f839
Noah Silvera <noah@super.gd>
2020-09-17 10:32:06 -0700

    Implement login action for auth controller
    Implement the basic login flow, including creating or updating a new rider
    based on the strava user_id and setting a session variable for the logged in rider.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,15 +3,34 @@ require "net/http"
 
 class AuthController < ApplicationController
   def login
+    unless params[:scope]&.include?("read") && params[:scope]&.include?("activity:read")
+      return render json: {message: "The application requires access to reading your profile and activities"}, status: :unauthorized
+    end
+
     token_params = {
       client_secret: Rails.application.credentials.strava_client_secret,
       client_id: "22020",
       code: params[:code]
     }
 
-    response = JSON.parse Net::HTTP.post_form(URI.parse('https://www.strava.com/oauth/token'), token_params).body
+    body = JSON.parse Net::HTTP.post_form(URI.parse("https://www.strava.com/oauth/token"), token_params).body
 
-    puts "Access Token: " + response["access_token"]
-    puts "Refresh Token: " + response["refresh_token"]
+    unless body && body["access_token"] && body["refresh_token"]
+      if body["errors"]
+        if body["errors"].any? { |error| error["resource"] == "AuthorizationCode" && error["code"] == "expired" }
+          return render json: {message: "Login authorization expired, please try again"}, status: :unauthorized
+        end
+      end
+
+      Rails.logger.error "Could not login with strava. Error: #{body}"
+      return render json: {message: "Could not login with strava for an unknown reason"}, status: :internal_server_error
+    end
+
+    rider = Rider.find_or_initialize_by(user_id: body["athlete"]["id"])
+    rider.update!(athlete_json: body["athlete"], access_token: body["access_token"], refresh_token: body["refresh_token"])
+    rider.save!
+
+    session[:rider_id] = rider.user_id
+    render json: {id: rider.user_id}, status: :ok
   end
 end
