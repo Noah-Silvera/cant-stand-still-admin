d4dd72c7686289dd2a103cda6bc1186d5004accb
Noah Silvera <noah@super.gd>
2020-09-17 09:25:14 -0700

    Enable pretty server side logging

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# TODO - improve this poor logging
+Rails.logger = Logger.new(STDOUT)
+Rails.application.config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
+Rails.logger.formatter = proc do | severity, time, progname, msg |
+  "#{time}, #{severity}: #{msg} from #{progname} \n"
+end
