0f5a9c70e9f7ffab2be339ef949fe7554cb793bf
Noah Silvera <noah@super.gd>
2020-09-28 09:15:37 -0700

    Configure factory bot
    Setup the factory bot gem for use with rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
