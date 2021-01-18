7b61ffd3274f23f41934175b6bf12deb8523c719
Noah Silvera <noah@super.gd>
2021-01-18 08:41:40 -0800

    Fix association of rider with rides

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,5 +1,5 @@
 class Rider < ApplicationRecord
-  has_many :rides
+  has_many :rides, foreign_key: :user_id
   has_many :trips, primary_key: :user_id, foreign_key: :user_id
   encrypts :access_token, :refresh_token
 
