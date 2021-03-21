c2ef410ccedd5a8fb62d700d011b246982c4fb92
Noah Silvera <noah@super.gd>
2021-03-21 14:05:23 -0700

    Run database seeds with bin/setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Rider.create!(
+rider = Rider.create!(
   user_id: 1,
   refresh_token: "fake_refresh_token",
   access_token: "fake_access_token",
@@ -8,7 +8,7 @@ Rider.create!(
   }
 )
 
-rider = Rider.create!(
+Rider.create!(
   user_id: 2,
   refresh_token: "fake_refresh_token",
   access_token: "fake_access_token",
@@ -19,9 +19,9 @@ rider = Rider.create!(
 )
 
 rides = [
-  Ride.create!(rider: rider),
-  Ride.create!(rider: rider),
-  Ride.create!(rider: rider)
+  Ride.create!(rider: rider, start_date: 2.hours.ago, activity_json: {}, activity_id: 1),
+  Ride.create!(rider: rider, start_date: 1.hours.ago, activity_json: {}, activity_id: 2),
+  Ride.create!(rider: rider, start_date: Time.zone.now, activity_json: {}, activity_id: 3)
 ]
 
 Trip.create! name: "Test Bike Trip", rides: rides, rider: rider
