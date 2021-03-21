8b641916d49fbfb276bab8023794a0a82f7dd794
Noah Silvera <noah@super.gd>
2021-03-21 09:12:05 -0700

    Make rides and trips dependent destroy
    If a rider is destroyed, their rides and trips should be too.

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,6 +1,6 @@
 class Rider < ApplicationRecord
-  has_many :rides, foreign_key: :user_id
-  has_many :trips, primary_key: :user_id, foreign_key: :user_id
+  has_many :rides, foreign_key: :user_id, dependent: :destroy
+  has_many :trips, primary_key: :user_id, foreign_key: :user_id, dependent: :destroy
   encrypts :access_token, :refresh_token
 
   def first_name
