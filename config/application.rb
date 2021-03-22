b183e46b7893472c5cbd78323cad357c5ef93c57
Noah Silvera <noah@super.gd>
2021-03-21 18:37:42 -0700

    Use redis URL if available
    This allows us to connect to redis in production

--- config/application.rb
+++ config/application.rb
@@ -44,11 +44,11 @@ module Api
     config.active_job.queue_adapter = :sidekiq
 
     Sidekiq.configure_server do |config|
-      config.redis = { url: 'redis://localhost:6379/0', namespace: "bike_fast" }
+      config.redis = { url: ENV.fetch("REDIS_URL") { 'redis://localhost:6379/0' }, namespace: "bike_fast" }
     end
-    
+
     Sidekiq.configure_client do |config|
-      config.redis = { url: 'redis://localhost:6379/0', namespace: "bike_fast" }
+      config.redis = { url: ENV.fetch("REDIS_URL") { 'redis://localhost:6379/0' }, namespace: "bike_fast" }
     end
   end
 end
