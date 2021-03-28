ac6600d2a12c17e7b394a6d2026896e506d53daa
Noah Silvera <noah@super.gd>
2021-03-27 17:13:57 -0700

    Serialize map function for ride
    We want to be able to return the map json for a ride when we serialize
    it as json.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,10 @@
 class Ride < ApplicationRecord
   belongs_to :rider, foreign_key: :user_id
   belongs_to :trip, optional: true
+
+  serialize :map, JSON
+
+  def map
+    activity_json["map"]
+  end
 end
