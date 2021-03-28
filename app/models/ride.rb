f27747cd5bb8adf895fb5ec0030224a9d4fcffb0
Noah Silvera <noah@super.gd>
2021-03-27 21:46:58 -0700

    Return distance and average speed of ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,14 +3,37 @@ class Ride < ApplicationRecord
   belongs_to :trip, optional: true
 
   serialize :map, JSON
+  serialize :name, JSON
+  serialize :distance_km, JSON
+  serialize :average_speed, JSON
 
   class << self
     def allowed_attributes
-      [:id, :start_date, :map, :user_id]
+      [
+        :id,
+        :start_date,
+        :map,
+        :user_id,
+        :name,
+        :distance_km,
+        :average_speed
+      ]
     end
+  end
+
+  def name
+    activity_json["name"]
   end
 
   def map
     activity_json["map"]
+  end
+
+  def distance_km
+    activity_json["distance"] / 1000.0
+  end
+
+  def average_speed
+    activity_json["average_speed"]
   end
 end
